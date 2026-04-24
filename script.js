f806277f30b35072ef760237589e2577753da021
b-paramesh <bhupathipramesh2025@gmail.com>
2026-04-24 21:12:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 document.querySelectorAll("a[href^='#']").forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
+anchor.addEventListener("click", function(e) {
+e.preventDefault();
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({ behavior: "smooth" });
+});
+});
+
+const faders = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+faders.forEach(el => {
+const top = el.getBoundingClientRect().top;
+if (top < window.innerHeight - 100) {
+el.classList.add("show");
+}
+});
 });
